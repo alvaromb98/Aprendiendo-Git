a3645c7d3af46a70097381f2059956e820a0f085
Álvaro Martín <alvaro.martin@innocv.com>
2021-12-23 12:48:16 +0100

    ruta hola añadida

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static(_dirname + '/public'));
 
+app.get('/hola', (req, res) => {
+    res.send('ruta Hola');
+});
+
 app.listen(3000, () => {
     console.log('Example app listening on port 3000');
 });
